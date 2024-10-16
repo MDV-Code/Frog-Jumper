aa2399eea82bb00ab51f6c14786af92a46322b62
Darko-HH <darkohh@gmail.com>
2024-10-16 13:15:08 +0200

    test

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ screen.bgcolor("white")
 frog = Frogger(screen)
 steuerung = Steuerung(screen)
 steuerung.controller(screen)
-
+ 
 screen.update()
 game_is_on = True
 last_movement =["n","s","e","w"]  #Nord,Süd, Ost, West 
